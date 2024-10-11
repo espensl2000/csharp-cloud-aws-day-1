953149877926e774a98fb504e6bdb92e9abad8f5
Espen Luna <espensluna@gmail.com>
2024-10-11 11:10:13 +0200

    Done

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,6 @@
 import "react";
 
-const url = import.meta.env.VITE_ENDPOINT_URL || "http://localhost:5168";
+const url = import.meta.env.VITE_ENDPOINT_URL //|| "http://localhost:5168";
 
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
